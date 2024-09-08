3d4e05ff27abcfc69f78859dd0245bde7e740168
MaureenMOguche <career.oguchemaureen@outlook.com>
2024-09-09 00:53:39 +0100

    requirement 1

--- Program.cs
+++ Program.cs
@@ -1,4 +1,60 @@
-﻿static void Main(string[] args)
+﻿namespace nex4;
+
+
+internal class Program
 {
-    
+    static void Main(string[] args)
+    {
+
+        Add();
+    }
+
+
+    public static void Add()
+    {
+        while(true)
+        {
+            List<int> nums = new List<int>();
+
+            Console.WriteLine("Enter two numbers separated by comma to add");
+
+            string input = Console.ReadLine();
+
+            if (string.IsNullOrEmpty(input))
+            {
+                Console.WriteLine($"Sum of {string.Join(",", input)} is 0");
+            }
+
+            var stringNumbers = input.Replace(" ", "");
+
+            if (stringNumbers.ToLower() == "x")
+            {
+                break;
+            }
+
+            var numbers = stringNumbers.Split(',');
+
+            if (numbers.Length > 2)
+            {
+                throw new InvalidOperationException("Please provide only two numbers");
+            }
+
+            foreach (var number in numbers)
+            {
+                if (!int.TryParse(number, out _))
+                {
+                    continue;
+                }
+                else
+                {
+                    nums.Add(int.Parse(number));
+                }
+            }
+
+            var sum = nums.Sum();
+            Console.WriteLine($"Sum of {string.Join(",", nums)} is {sum}");
+
+
+        }
+    }
 }
